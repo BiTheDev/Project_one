576c3aa6ad518a0b692be47982aa7fdc46a562cd
BrianHo <Curry30@Brians-MacBook-Pro-3.local>
2018-07-23 11:44:36 -0700

    urls and view files

--- apps/Food_Truck/views.py
+++ apps/Food_Truck/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+	return render(request, 'home.html')
+
+def login(request):
+	if trucks < 1:
+		return redirect('/create/')
+
+	return redirect('/dashboard/')
+
+def create(request):
+
+	return render(request, 'first_store.html')
+
+def create_truck(request):
+
+	return redirect('/dashboard/')
+
+def dashboard(request):
+
+	return render(request, 'dashboard.html')
